2ca7e96a93629c241191754d47e9bf8f3a6a1362
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-04-02 16:02:58 -0700

    added samclip

--- scripts/run_minimap.sh
+++ scripts/run_minimap.sh
@@ -10,5 +10,8 @@ READS=$1
 REF=$2
 MAPPING=$3
 
-minimap2 -x map-hifi -t12 -a $REF $READS | samtools view -b | samtools sort > $MAPPING
+SAMCLIP=/Users/gabrielraulet/Development/overlap-to-contig-prototype/scripts/samclip
+
+samtools faidx $REF
+minimap2 -x map-hifi -t12 -a $REF $READS | $SAMCLIP --ref $REF --max 100 | samtools view -b | samtools sort > $MAPPING
 samtools index $MAPPING
